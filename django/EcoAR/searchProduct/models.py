f8df3ec1ce55ba97fa5e31083703b30a80624d7a
MaxHernandez <MaxHernandezCastillo@gmail.com>
2014-08-13 23:40:03 -0500

    Modificaciones en el modelo de la base de datos

--- django/EcoAR/searchProduct/models.py
+++ django/EcoAR/searchProduct/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Producto (models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
-    marca = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50, null=True)
     fabricante =  models.CharField(max_length=50)
     codigo_barras = models.CharField(max_length=20)
     imagen = models.ImageField(upload_to='/products/', null=True)
