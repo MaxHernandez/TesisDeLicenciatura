3df9e91fb45f97c5a10049c75cff1c5985dffee0
MaxHernandez <MaxHernandezCastillo@gmail.com>
2014-11-18 22:49:07 -0600

    Se agrego funcionalidad para modificar los simbolos de aprovado o no aprovado de las categorias en la muestra de informacion de un producto.

--- EcoAR/src/com/maxkalavera/ecoar/productinfo/ProductInfo.java
+++ EcoAR/src/com/maxkalavera/ecoar/productinfo/ProductInfo.java
@@ -11,7 +11,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.graphics.BitmapFactory;
+import 	android.graphics.Bitmap;
 
 public class ProductInfo extends BaseActivity {
 
@@ -23,6 +24,33 @@ public class ProductInfo extends BaseActivity {
 		((TextView) findViewById(R.id.productinfo_productname)).setText(info.product.productName);
 		((ImageView) findViewById(R.id.productinfo_productimage)).setImageBitmap(info.product.image);
 		((TextView) findViewById(R.id.productinfo_productqualification)).setText(info.qualification);
+
+		Bitmap aproved = BitmapFactory.decodeResource(getResources(), R.drawable.aproved);
+		Bitmap notAproved = BitmapFactory.decodeResource(getResources(), R.drawable.notaproved);		
+		
+		boolean categoriesBooleanList[] = new boolean[] {
+				info.transport,
+				info.energy,
+				info.water,
+				info.society,
+				info.recyclable
+		};
+		int categoriesIDList[] = new int[] {
+				R.id.productinfo_transport,
+				R.id.productinfo_energy,
+				R.id.productinfo_water,
+				R.id.productinfo_society,
+				R.id.productinfo_recyclable
+		};
+		
+		for (int i = 0; i < categoriesBooleanList.length; i++ ) {
+			if (categoriesBooleanList[i]) {
+				((ImageView) findViewById(categoriesIDList[i])).setImageBitmap(aproved);
+			} else {
+				((ImageView) findViewById(categoriesIDList[i])).setImageBitmap(notAproved);
+			}
+		}
+
 	}
 	
 
